50bdbc3e7b81ee7a814732fd8ff92f7f4c3b829c
Hidenori Shinohara <shino013@umn.edu>
2016-01-01 22:08:30 -0600

    add main.rb

--- checker/main.rb
+++ checker/main.rb
@@ -17,10 +17,19 @@ def run_checker user_solution
   end
 
   # Replace $s in Checker.java to user_solution
-  system( "sed -i 's/$s/" + user_solution + "/g' Checker.java")
-  if !$?.success?
-    raise "Error while inserting user's solution into Checker.java"
-  end
+
+
+#############
+  file_name = '/tmp/Checker.java'
+  text = File.read(file_name)
+  new_contents = text.gsub("$s", user_solution)
+  File.open(file_name, "w") {|file| file.puts new_contents }
+#############
+
+#  system( "sed -i 's/$s/" + user_solution + "/g' Checker.java")
+#  if !$?.success?
+#    raise "Error while inserting user's solution into Checker.java"
+#  end
 
   # Compiling...
   system 'javac Checker.java'
