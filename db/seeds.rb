861def4433d54a26e9ea1bf908ee64f7bed52283
Yancy Ribbens <yancy.ribbens@gmail.com>
2016-01-09 21:23:48 -0600

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Problem.create(output: "hello!", desc: "return a given string with ! appended at the end", input:"hello", func_type: "String", signature: "String myfunc(String s) { return null; }")
+Problem.create(output: "3", desc: "Return the given input + 1", input: "2", func_type: "int", signature: "int myfunc(int x) { return x + 1; }")
